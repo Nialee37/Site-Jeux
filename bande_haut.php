7a3c52b98e64e74833fffb9b7943544a5de009fb
Nialee37 <43949445+Nialee37@users.noreply.github.com>
2019-05-21 18:27:00 +0200

    Delete bande_haut.php

--- bande_haut.php
+++
@@ -1,7 +0,0 @@
-<p class="titre">
-	<img  style="padding-top: 1%; padding-bottom: 1%; " src="Images\titre.png" alt=""/>
-	<a href="page_accueil.php"><img class="accueil" src="Images\logo maison.png" alt=""/>
-	<a href="./index.php"><img class="connection" src="Images\connection.png" alt=""/>
-</p>
-
-		
